a58069dab7a1936542c8b36931511bf9a49768dc
shencangsheng <shencangsheng@126.com>
2022-05-11 10:50:43 +0800

    perf: delete alias version param

--- alias/lex-alias.sh
+++ alias/lex-alias.sh
@@ -2,15 +2,14 @@
 # creator by shencangsheng
 set -xe
 
-mysql_version="5.7"
 mysql_file=
 mysql_lex=
 mysql_lex_alias=
 
-while getopts "v:f:c:a:h" opt; do
+while getopts "v:f:c:h" opt; do
     case $opt in
     v)
-        mysql_version=$OPTARG
+        mysql_lex_alias=$OPTARG
         ;;
     f)
         mysql_file=$OPTARG
@@ -18,22 +17,18 @@ while getopts "v:f:c:a:h" opt; do
     c)
         mysql_lex=$OPTARG
         ;;
-    a)
-        mysql_lex_alias=$OPTARG
-        ;;
     h)
         echo "Add an Alias to Mysql System Lex"
         echo "Usage: bash alias.sh [OPTION...]"
         echo ""
         echo "Examples:"
-        echo "bash alias.sh -v 5.7 -f ~/mysql-5.7-server -c REGEXP -a REGEXP_ALIAS"
+        echo "bash alias.sh -f ~/mysql-5.7-server -c REGEXP -v REGEXP_ALIAS"
         echo ""
         echo "Main operation mode:"
         echo ""
-        echo "-v, Default 5.7 # Mysql version [Optional]"
         echo "-f, # Mysql server source file path [Required]"
         echo "-c, # To be copied mysql system lex [Required]"
-        echo "-a, # Mysql system lex alias [Required]"
+        echo "-v, # Mysql system lex alias [Required]"
         exit 0
         ;;
     *)
